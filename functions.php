4f826179d98dfb294e39a73110ad6829fadf8b73
alcogr <alcogr@gmail.com>
2018-12-07 16:15:31 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -20,4 +20,35 @@ function register_menus() {
   
   add_action( 'init', 'register_menus' ); // <-- of course without this, the function
 
+function current_page($title){
+    global $post;
+    if($title === get_the_title( $post->post_parent ) ) 
+        return "active";
+    return "";
+}
 
+function wp_get_menu_array($current_menu) {
+ 
+    $array_menu = wp_get_nav_menu_items($current_menu);
+    $menu = array();
+    foreach ($array_menu as $m) {
+        if (empty($m->menu_item_parent)) {
+            $menu[$m->ID] = array();
+            $menu[$m->ID]['ID']      =   $m->ID;
+            $menu[$m->ID]['title']       =   $m->title;
+            $menu[$m->ID]['url']         =   $m->url;
+            $menu[$m->ID]['children']    =   array();
+        }
+    }
+    $submenu = array();
+    foreach ($array_menu as $m) {
+        if ($m->menu_item_parent) {
+            $submenu[$m->ID] = array();
+            $submenu[$m->ID]['ID']       =   $m->ID;
+            $submenu[$m->ID]['title']    =   $m->title;
+            $submenu[$m->ID]['url']  =   $m->url;
+            $menu[$m->menu_item_parent]['children'][$m->ID] = $submenu[$m->ID];
+        }
+    }
+    return $menu;
+}
