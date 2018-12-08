f0e8e308f21b6856515d3e1a6bb1bb8d77347ab6
alcogr <alcogr@gmail.com>
2018-12-08 10:03:10 +0200

    Update single.php

--- single.php
+++ single.php
@@ -1,8 +1,7 @@
 <?php
 if ( have_posts() ) : 
     while ( have_posts() ) : the_post();
-    $permalink  = explode('/',get_the_permalink());
-    $size = count($permalink);
-    header("Location:".site_url()."/".get_post_type()."/?title=".$permalink[$size-2]);
+    $post_name = get_post(get_the_ID())->post_name;
+    header("Location:".site_url()."/".get_post_type()."/?title=".$post_name);
     endwhile;
 endif;
